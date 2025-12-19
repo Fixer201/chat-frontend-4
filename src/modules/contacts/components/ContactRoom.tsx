4f03e5f7a54bf8aaeca7a2b1652f46cc1266dda4
aleksandrova22 <lialia1986@mail.ru>
2025-12-19 21:41:55 +0700

    feat(ContactsList)+fix(Avatar)

--- src/modules/contacts/components/ContactRoom.tsx
+++ src/modules/contacts/components/ContactRoom.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import MessageComposer from '@modules/message-composer/components/MessageComposer'
-import ContactHeader from './ContactHeader'
 import MessagesList from '@modules/chat-room/components/MessagesList'
 import EmptyContactsState from './EmptyContactsState'
 import { useSelector } from 'react-redux'
@@ -9,7 +8,6 @@ import { RootState } from '../../../redux/store'
 import ChatHeader from '@modules/chat-room/components/ChatHeader';
 import { useChats } from '@shared/hooks/useChats';
 import { useEffect } from 'react';
-
 
 export default function ContactRoom() {
     const uid = useSelector(
