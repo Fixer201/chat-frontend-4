60b93f52a4390d1083e1c79dc120bcc2b93f1c0c
aleksandrova22 <lialia1986@mail.ru>
2025-12-24 20:15:58 +0700

    feat(RegisterForm)

--- src/app/(public)/auth/register/page.tsx
+++ src/app/(public)/auth/register/page.tsx
@@ -1,14 +1,16 @@
+import RegisterForm from "@modules/auth/components/RegisterForm";
+
 export default function RegisterPage() {
   return (
-    <div className="min-h-screen flex justify-center items-center">
+
+ <div className="min-h-screen flex justify-center items-center">
+      <div className="hidden md:flex w-300 h-256 bg-[url(/images/login/Background.svg)] loading=eager justify-center relative items-center flex-col">
+        <RegisterForm />
+      </div>
       
-      <form className="w-100 p-6 bg-white rounded-lg shadow-lg">
-        <h2 className="text-2xl mb-4">Регистрация</h2>
-       
-        <input type="email" placeholder="Email" className="w-full mb-4 p-2 border" />
-        <input type="password" placeholder="Пароль" className="w-full mb-4 p-2 border" />
-        <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded">Зарегистрироваться</button>
-      </form>
     </div>
+
+
+
   );
 }
