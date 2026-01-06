fc9418c7f0c9547550d99b61717d61c9c8cf922e
varavoi <institut93@gmail.com>
2026-01-06 13:34:49 +0300

    fix(customscrollbar):fixed bug with import

--- src/shared/ui/CustomScrollbar/CustomScrollbar.tsx
+++ src/shared/ui/CustomScrollbar/CustomScrollbar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useRef, useCallback } from 'react'
-import customStyle from '@shared/ui/CustomScrollbar/CustomScrollbar.module.css'
+import customStyle from '@shared/ui/customScrollbar/customScrollbar.module.css'
 export function CustomScrollbar({
     children,
     className = '',
