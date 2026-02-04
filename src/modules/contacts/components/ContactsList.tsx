7e610e1d05787ed694461bff9197a5c59c6cff59
aleksandrova22 <lialia1986@mail.ru>
2026-02-04 16:00:31 +0700

    fix(lint)

--- src/modules/contacts/components/ContactsList.tsx
+++ src/modules/contacts/components/ContactsList.tsx
@@ -422,10 +422,10 @@ export default memo(function ContactsList() {
                                     <>
                                         <div
                                             className={`
-                                          flex h-9 w-full justify-between gap-1
-                                          bg-accent-violet-ultra-light pt-2.5
-                                          pr-4 pb-2.5 pl-4
-                                        `}
+                                              flex h-9 w-full justify-between
+                                              gap-1 bg-accent-violet-ultra-light
+                                              pt-2.5 pr-4 pb-2.5 pl-4
+                                            `}
                                         >
                                             <p>
                                                 Контакты
