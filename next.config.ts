02800d4ef853ab3f08144ac70870ef06dd124c33
Ilya_R <95310782+IlyaRadu@users.noreply.github.com>
2026-02-12 11:46:58 +0100

    Add CORS headers for api.test.chat.ktsf.ru

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,10 @@ const nextConfig: NextConfig = {
                 protocol: 'https',
                 hostname: 'via.placeholder.com',
             },
+            {
+                protocol: 'https',
+                hostname: 'api.test.chat.ktsf.ru',
+            },
         ],
     },
     // Добавляем headers для CORS (чтобы разрешить запросы к /api/* из браузера)
