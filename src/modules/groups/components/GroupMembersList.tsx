582192c5a3458723cf832fa385580cb1fb5e9d6b
varavoi <institut93@gmail.com>
2026-03-05 13:47:12 +0300

    fix: fixed some bugs

--- src/modules/groups/components/GroupMembersList.tsx
+++ src/modules/groups/components/GroupMembersList.tsx
@@ -137,15 +137,16 @@ export default function GroupMembersList({
     return (
         <div
             className={`
-          flex h-(--screen-height-list) min-h-0 flex-col rounded-md bg-gray-main
-        `}
+              flex h-(--screen-height-list) min-h-0 flex-col rounded-md
+              bg-gray-main
+            `}
         >
             {/* Шапка */}
             <div
                 className={`
-              flex items-center justify-start gap-3 rounded-t-md border-b
-              border-app-divider bg-gray-main px-6 py-4
-            `}
+                  flex items-center justify-start gap-3 rounded-t-md border-b
+                  border-app-divider bg-gray-main px-6 py-4
+                `}
             >
                 <Button
                     onClick={onBack}
@@ -162,8 +163,8 @@ export default function GroupMembersList({
                 </Button>
                 <h2
                     className={`
-                  text-lg font-medium tracking-extra-tight text-text-black
-                `}
+                      text-lg font-medium tracking-extra-tight text-text-black
+                    `}
                 >
                     Пригласить участников
                 </h2>
